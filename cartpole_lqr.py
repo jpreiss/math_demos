568caa1da1c7de140b7dfd02eb5596599169400e
jpreiss <jamesalanpreiss@gmail.com>
2021-04-16 14:54:18 -0700

    refactor cartpole_lqr

--- cartpole_lqr.py
+++ cartpole_lqr.py
@@ -25,103 +25,125 @@ dth = 0
 x = -1
 dx = 0
 
-# forward integrate dynamics over time interval dt
+
 def dynamics(th, dth, x, dx, force, dt):
-	sth = np.sin(th)
-	cth = np.cos(th)
+    """Integrate nonlinear dynamics of cart-pole system."""
+    sth = np.sin(th)
+    cth = np.cos(th)
 
-	# ODEs
-	ddth = ((g*sth + cth*(-force - mp*l*dth**2*sth)/(mc + mp))
-		 / (l*(4/3 - (mp*cth**2)/(mc + mp))))
+    # ODEs
+    ddth = ((g*sth + cth*(-force - mp*l*dth**2*sth)/(mc + mp))
+         / (l*(4/3 - (mp*cth**2)/(mc + mp))))
 
-	ddx = (force + mp*l*(dth**2*sth - ddth*cth))/(mc + mp)
+    ddx = (force + mp*l*(dth**2*sth - ddth*cth))/(mc + mp)
 
-	# noise
-	ddth += 0.4 * np.random.normal()
-	ddx += 0.4 * np.random.normal()
+    # noise
+    ddth += 0.4 * np.random.normal()
+    ddx += 0.4 * np.random.normal()
 
-	# euler integration
-	dth = dth + dt * ddth
-	th = th + dt * dth
-	dx = dx + dt * ddx
-	x = x + dt * dx
+    # euler integration
+    dth = dth + dt * ddth
+    th = th + dt * dth
+    dx = dx + dt * ddx
+    x = x + dt * dx
 
-	return th, dth, x, dx
+    return th, dth, x, dx
 
-# compute LQR linear feedback control gain matrix.
-# A, B: continuous system dynamics dx/dt = Ax + Bu
-# Q, R: quadratic cost J = x'Q x + u'R u
+
 def lqr(A, B, Q, R):
-	# use SciPy to solve cts time algebraic Riccati eqn
-	B = B.reshape((B.size, 1))
-	P = sp.linalg.solve_continuous_are(A, B, Q, R)
-	K = -np.linalg.solve(R, np.dot(B.T, P))
-	return K
+    """Compute LQR optimal feedback control gains.
+
+    Args:
+        A, B: Continuous system dynamics dx/dt = Ax + Bu.
+        Q, R: Quadratic cost J(t) = x'Q x + u'R u.
+
+    Returns:
+        K: Gain matrix.
+    """
+    B = B.reshape((B.size, 1))
+    P = sp.linalg.solve_continuous_are(A, B, Q, R)
+    K = -np.linalg.solve(R, np.dot(B.T, P))
+    return K
+
+
+def cartpole_linear():
+    """Construct linearized dynamics matrices for the cart-pole system.
+
+    Derivation:
+
+    ddth = (g*0 + 1*(-force - 0)/(mc + mp) / (l*(4/3 - mp/(mc+mp)))
+         = C * force, where C = -1/(mc+mp) / (l*4/3 - mp/(mc+mp))
+    ddx = (force + mp*l*(0-ddth)/(mc + mp)
+        = force - mp*l*C*force/(mc+mp)
+        = (1 - mp*l*C/(mc+mp)) * force = C2 * force
+
+    s = [th dth x dx]
+    ds/dt = As + Bforce, where:
+    A = [0    1 0 0
+         grav 0 0 0
+         0    0 0 1
+         0    0 0 0],
+
+    B = [0 C 0 C2].
+
+    Returns:
+        A, B: Dynamics matrices such that $\\dot x \\approx Ax + Bu$.
+    """
+
+    A = np.array([
+        [0,    1, 0, 0],
+        [9.81, 0, 0, 0],
+        [0,    0, 0, 1],
+        [0,    0, 0, 0]
+    ])
+    C1 = (-1/(mc+mp)) / (l*4/3 - mp/(mc+mp))
+    C2 = 1 - mp*l*C1/(mc+mp)
+    B = np.array([0, C1, 0, C2])
+    return A, B
+
+
+def simulate(K):
+    # for rendering
+    fps = 30
+    dt = 1.0 / fps
+    cart_w = 0.3
+    cart_h = 0.15
+    track_limit = 2.4
+    xlimits = [-track_limit, track_limit]
+
+    secs = 5
+    frames = fps * secs
+
+    fig, ax = plt.subplots()
+    line, = ax.plot([0, 0], [1, 1])
+    rect = mpl.patches.Rectangle((0, 0), cart_w, cart_h)
+    ax.add_patch(rect)
+    ax.axis("equal")
+    ax.set_xlim(xlimits)
+    ax.set_ylim([-.2*l, 2.2*l])
+
+    def anim_fn(iframe):
+        global x, dx, th, dth
+        line.set_xdata([x, x + 2*l*np.sin(th)])
+        line.set_ydata([0, 2*l*np.cos(th)])
+        rect.set_xy((x-cart_w/2, -cart_h/2))
+        force = np.dot(K, np.array([th, dth, x, dx]))[0]
+        th, dth, x, dx = dynamics(th, dth, x, dx, force, dt)
+        return line, rect
+
+    ani = mpl.animation.FuncAnimation(fig, anim_fn,
+        interval=1000*dt, frames=frames, repeat=False,
+    )
+    plt.show()
+
 
 def main():
+    A, B = cartpole_linear()
+    Q = np.diag([1, 0.1, 1, 0.1])
+    R = 0.001 * np.eye(1)
+    K = lqr(A, B, Q, R)
+    simulate(K)
 
-	"""
-	linearization about equilibrium point of 0:
-	ddth = (g*0 + 1*(-force - 0)/(mc + mp) / (l*(4/3 - mp/(mc+mp)))
-	     = C * force, where C = -1/(mc+mp) / (l*4/3 - mp/(mc+mp))
-	ddx = (force + mp*l*(0-ddth)/(mc + mp)
-	    = force - mp*l*C*force/(mc+mp)
-	    = (1 - mp*l*C/(mc+mp)) * force = C2 * force
-	
-	s = [th dth x dx]
-	ds/dt = As + Bforce, where:
-	A = [0    1 0 0
-	     grav 0 0 0
-	     0    0 0 1
-	     0    0 0 0]
-	
-	B = [0 C 0 C2]
-	"""
 
-	C1 = (-1/(mc+mp)) / (l*4/3 - mp/(mc+mp))
-	C2 = 1 - mp*l*C1/(mc+mp)
-	A = np.array([
-		[0,    1, 0, 0],
-		[9.81, 0, 0, 0],
-		[0,    0, 0, 1],
-		[0,    0, 0, 0]
-	])
-	B = np.array([0, C1, 0, C2])
-	Q = np.diag([1, 0.1, 1, 0.1])
-	R = 0.001 * np.eye(1)
-	K = lqr(A, B, Q, R)
-
-	# for rendering
-	fps = 30
-	dt = 1.0 / fps
-	cart_w = 0.3
-	cart_h = 0.15
-	track_limit = 2.4
-	xlimits = [-track_limit, track_limit]
-
-	secs = 5
-	frames = fps * secs
-
-	fig, ax = plt.subplots()
-	line, = ax.plot([0, 0], [1, 1])
-	rect = mpl.patches.Rectangle((0, 0), cart_w, cart_h)
-	ax.add_patch(rect)
-	ax.axis("equal")
-	ax.set_xlim(xlimits)
-	ax.set_ylim([-.2*l, 2.2*l])
-
-	def anim_fn(iframe):
-		global x, dx, th, dth
-		line.set_xdata([x, x + 2*l*np.sin(th)])
-		line.set_ydata([0, 2*l*np.cos(th)])
-		rect.set_xy((x-cart_w/2, -cart_h/2))
-		force = np.dot(K, np.array([th, dth, x, dx]))[0]
-		th, dth, x, dx = dynamics(th, dth, x, dx, force, dt)
-		return line, rect
-
-	ani = mpl.animation.FuncAnimation(fig, anim_fn,
-		interval=1000*dt, frames=frames, repeat=False,
-	)
-	plt.show()
-
-main()
+if __name__ == "__main__":
+    main()
