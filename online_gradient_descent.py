622f162c5e43c13c93108f997488f99ed958260e
jpreiss <jamespalanpreiss@gmail.com>
2025-08-06 13:05:44 -0700

    2up video

--- online_gradient_descent.py
+++ online_gradient_descent.py
@@ -1,7 +1,4 @@
-"""Illustrate how online gradient descent handles nonstationary environments.
-"""
-
-import sys
+"""Illustrate how online gradient descent handles nonstationary environments."""
 
 import matplotlib.animation
 import matplotlib.pyplot as plt
@@ -10,49 +7,73 @@ import seaborn as sns
 
 
 # Use continuous time so we can tweak at low FPS then render at high FPS.
-RATE = 1.0
-PERIOD = 6.0
-LAPS = 2
-FPS = 4
+RATE = 2.0
+PERIODS = np.array([8.0, 1.0])
+LAPS = 1
+FPS = 12
 DT = 1.0 / FPS
 
 
-def main():
-    T = int(FPS * LAPS * PERIOD)
-    OMEGA = 2 * np.pi / PERIOD
-    x = np.zeros(2)
+class OGDPlot:
+    def __init__(self, ax, omega, T):
+        self.omega = omega
+        self.xs = np.zeros((T, 2))
+        self.xopts = np.zeros((T, 2))
+        self.xs_plot = ax.plot([], [], label="$y_t$ (ALG)", color="black")[0]
+        self.xopts_plot = ax.plot([], [], label="$y^\\star_t$", alpha=0.25, color="black")[0]
+        box = 1.6
+        ax.set(xlim=[-box, box], ylim=[-box, box])
+        ax.axis("equal")
+        sns.despine(ax=ax, bottom=True, left=True)
+        ax.set(xticks=[], yticks=[])
+        self.x = np.zeros(2)
 
-    xs = np.zeros((T, 2))
-    xopts = np.zeros((T, 2))
+    def step(self, i):
+        self.xs_plot.set_data(self.xs[:i, 0], self.xs[:i, 1])
+        self.xopts_plot.set_data(self.xopts[:i, 0], self.xopts[:i, 1])
+        theta = self.omega * DT * i
+        xopt = np.array([np.cos(theta), np.sin(theta)])
+        x = self.x
+        self.xs[i] = x
+        self.xopts[i] = xopt
+        grad = x - xopt
+        self.x = x - DT * RATE * grad
+
+
+def main():
+    T = int(FPS * LAPS * PERIODS[0]) + 1
+    OMEGAS = 2 * np.pi / PERIODS
+
+    xs = np.zeros((2, T, 2))
+    xopts = np.zeros((2, T, 2))
 
     plt.rcParams["text.usetex"] = True
-    fig, ax = plt.subplots(1, 1, figsize=(4.0, 4.0), dpi=200)
-    opt_trace, = ax.plot([], [], label="$y^\\star_t$", alpha=0.25, color="black")
-    x_trace, = ax.plot([], [], label="$y_t$ (ALG)", color="black")
-    ax.legend()
-    ax.set_title("$h_t(y) = \\|y - y^\\star_t\\|_2^2$")
-    box = 1.5
-    ax.set(xlim=[-box, box], ylim=[-box, box])
-    ax.axis("equal")
-    sns.despine(ax=ax, bottom=True, left=True)
-    ax.set(xticks=[], yticks=[])
+    fig, axs = plt.subplots(1, 2, figsize=(8.0, 4.0), constrained_layout=True, dpi=200)
+    plots = [OGDPlot(ax, omega, T) for ax, omega in zip(axs, OMEGAS)]
+    axs[0].set_title("slow-moving target")
+    axs[1].set_title("fast-moving target")
+
+    # move legend to in-between
+    axs[0].legend()
+    legend = axs[0].get_legend_handles_labels()
+    fig.legend(*legend, loc='upper center', fontsize="large")
+    fig.text(
+        0.5, 0.8,
+        "$f_t(y_t) = \\|y_t - y^\\star_t\\|_2^2$",
+        ha="center", va="top", fontsize="large",
+    )
+    axs[0].get_legend().remove()
 
     writer = matplotlib.animation.FFMpegWriter(fps=FPS, bitrate=100*FPS)
     writer.setup(fig, "ogd.mp4")
 
     for i in range(T):
-        x_trace.set_data(xs[:i, 0], xs[:i, 1])
-        opt_trace.set_data(xopts[:i, 0], xopts[:i, 1])
+        for plot in plots:
+            plot.step(i)
         writer.grab_frame()
-        plt.show(block=False)
-        plt.pause(1e-2)
-
-        theta = OMEGA * DT * i
-        xopt = np.array([np.cos(theta), np.sin(theta)])
-        xs[i] = x
-        xopts[i] = xopt
-        grad = x - xopt
-        x = x - DT * RATE * grad
+        if FPS < 60:
+            plt.show(block=False)
+            plt.pause(1e-2)
 
     writer.finish()
 
